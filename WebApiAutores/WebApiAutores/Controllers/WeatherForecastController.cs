fc0a745350db3f09f99a0f7ce0c922e1409f7481
Daniel.Garcia <sanck.one@gmail.com>
2022-05-02 13:55:58 -0700

    Testing Debugging DateTime and see value

--- WebApiAutores/WebApiAutores/Controllers/WeatherForecastController.cs
+++ WebApiAutores/WebApiAutores/Controllers/WeatherForecastController.cs
@@ -21,6 +21,7 @@ namespace WebApiAutores.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            var hoy = DateTime.Now;
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
